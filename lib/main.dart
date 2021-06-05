4d7323137d2b3847397f34d6217da9d917ca2926
Satyam parasa <sathyam.parasa@gmail.com>
2021-06-05 16:17:51 +0530

    code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scratcher/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,24 +27,81 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scratch Card"),
+        title: Text(
+          "Scratch Card",
+        ),
       ),
       body: Container(
         child: Center(
-          child: TextButton(
-            onPressed: () {
-              onRewardClicked();
-            },
-            style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.teal,
-                textStyle: TextStyle(color: Colors.red)),
-            child: Text("Reward"),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            height: MediaQuery.of(context).size.height * 0.1,
+            child: TextButton(
+              onPressed: () {
+                showScratchCard(context);
+              },
+              style: TextButton.styleFrom(
+                  textStyle:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue),
+              child: Text("Get Reward"),
+            ),
           ),
         ),
       ),
     );
   }
 
-  void onRewardClicked() {}
+  showScratchCard(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Scratcher(
+              brushSize: 100,
+              threshold: 50,
+              color: Colors.blue,
+              onChange: (value) => print("Scratch progress: $value%"),
+              onThreshold: () => print("Threshold reached"),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.32,
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Image.asset(
+                        "assets/cele.png",
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "You\'ve won",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24,
+                            letterSpacing: 1,
+                            color: Colors.blue),
+                      ),
+                    ),
+                    Text(
+                      "\$10",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 24,
+                          color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
 }
